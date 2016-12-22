ecd062e132ebb79db721207541d757dc67f4ff77
Phil <philneedham85@gmail.com>
2016-12-22 23:46:51 +0000

    Initial setup

---
+++ DebouncingTest/CustomCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DebouncingTest
+{
+    public class CustomCommand : ICommand
+    {
+        public delegate void CommandOnExecute();
+        public delegate bool CommandOnCanExecute(object parameter);
+
+        private readonly CommandOnExecute _execute;
+        private readonly CommandOnCanExecute _canExecute;
+
+        public CustomCommand(CommandOnExecute onExecuteMethod, CommandOnCanExecute onCanExecuteMethod)
+        {
+            _execute = onExecuteMethod;
+            _canExecute = onCanExecuteMethod;
+        }
+
+        public CustomCommand(CommandOnExecute onExecuteMethod) : this(onExecuteMethod, (obj) => true)
+        {
+        }
+
+        #region ICommand Members
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute.Invoke(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke();
+        }
+
+        #endregion
+    }
+}
---
+++ DebouncingTest/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace DebouncingTest
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private int _commandsSent;
+
+        public int CommandsSent
+        {
+            get { return _commandsSent; }
+            set
+            {
+                _commandsSent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand Button1Command { get; set; }
+        public ICommand Button2Command { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Button1Command = new CustomCommand(SendCommand);
+            Button2Command = new CustomCommand(SendCommand);
+        }
+
+        private void SendCommand()
+        {
+            CommandsSent++;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
